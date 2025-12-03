ae9aa8f33974930095cba8cd211c1e11eafdbf2f
TheIllumi <saadmughal321@gmail.com>
2025-12-03 11:47:00 +0500

    Use Case 8 Implemented & Tested

--- src/main/java/io/saadmughal/assignment05/repository/AccountsRepository.java
+++ src/main/java/io/saadmughal/assignment05/repository/AccountsRepository.java
@@ -4,5 +4,8 @@ import io.saadmughal.assignment05.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface AccountsRepository extends JpaRepository<Accounts, Long>, JpaSpecificationExecutor<Accounts> {
+    List<Accounts> findByUserId(Long userId);
 }
